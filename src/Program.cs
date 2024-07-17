0abc29f524ec83bddda38ca21e0f8c51129ce608
Wesley Perumal <perumal.wesley@gmail.com>
2024-07-18 00:03:50 +0200

    update pull policy

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,7 @@ var config = builder.Configuration;
 var app = builder.Build();
 var bootstrapUrl = "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">";
 
-app.MapGet("/", () => $"Hello World! from {Environment.MachineName}");
+app.MapGet("/", () => new CustomHTMLResult($"<html>{bootstrapUrl}<body class=\"{config["Settings:backgroundClass"]}\"><h1>Hello World! from <span>{Environment.MachineName}</span></body></html>"));
 
 app.MapGet("/write", (IConfiguration config) =>
 {
