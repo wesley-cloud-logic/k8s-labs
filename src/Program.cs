4c0607c2c8a380493973c864e4e0c3049922a914
Wesley Perumal <perumal.wesley@gmail.com>
2024-07-18 10:17:00 +0200

    Added styling

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,12 @@ builder.Configuration.AddEnvironmentVariables();
 var config = builder.Configuration;
 var app = builder.Build();
 var bootstrapUrl = "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">";
+var navbar = $"<nav class=\"{config["Settings:navBackgroundClass"]} navbar\">"
++ "<div class=\"container-fluid\">"
++ $"<a class=\"navbar-brand\" href=\"#\">{config["Settings:siteName"]}</a></div></nav>";
 
-app.MapGet("/", () => new CustomHTMLResult($"<html>{bootstrapUrl}<body class=\"{config["Settings:backgroundClass"]}\"><h1>Hello World! from <span>{Environment.MachineName}</span></body></html>"));
+
+app.MapGet("/", () => new CustomHTMLResult($"<html>{bootstrapUrl}{navbar}<body class=\"{config["Settings:backgroundClass"]}\"><h1 class=\"p-3\">Hello World! from <span>{Environment.MachineName}</span></body></html>"));
 
 app.MapGet("/write", (IConfiguration config) =>
 {
@@ -18,7 +22,7 @@ app.MapGet("/write", (IConfiguration config) =>
 
     database.StringSet("timeOfWrite", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
 
-    return new CustomHTMLResult($"<html>{bootstrapUrl}<span>View Data <a href='/read'>here</a></span></html>");
+    return new CustomHTMLResult($"<html>{bootstrapUrl}{navbar}<span>View Data <a href='/read'>here</a></span></html>");
 });
 
 app.MapGet("/read", (IConfiguration config) =>
@@ -27,19 +31,22 @@ app.MapGet("/read", (IConfiguration config) =>
     var database = redis.GetDatabase();
     var value = database.StringGet("timeOfWrite");
 
-    return new CustomHTMLResult($"<html>{bootstrapUrl}<h1>Database Read Success</h1><span>Last time data was written was at {value}</span></html>");
+    return new CustomHTMLResult($"<html>{bootstrapUrl}{navbar}<h1>Database Read Success</h1><span>Last time data was written was at {value}</span></html>");
 });
 
 app.MapGet("/config", (IConfiguration config) =>
 {
     var confKeys = config.AsEnumerable().ToList();
     var stringBuilder = new StringBuilder();
-    stringBuilder.Append(bootstrapUrl);
 
+    stringBuilder.Append("<html>");
+    stringBuilder.Append(bootstrapUrl);
+    stringBuilder.Append(navbar);
     stringBuilder.Append("<table class=\"table table-striped\"><thead><tr><th>Name</th><th>Value</th><tbody>");
     confKeys.ForEach(kv => stringBuilder.Append($"<tr><td>{kv.Key}</td><td>{kv.Value}</td></tr>"));
 
     stringBuilder.Append("</tbody></table>");
+    stringBuilder.Append("</html>");
 
     return new CustomHTMLResult($"<h1>Config Values</h1> {stringBuilder.ToString()}");
 });
